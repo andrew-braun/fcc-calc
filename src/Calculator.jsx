2d83d842d0e00c50d7ed8ed72496e6327d7101eb
Andrew Braun <andrew@andrewbraun.dev>
2021-01-20 22:39:56 +0400

    feat: Add basic calculator display logic

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,78 +1,151 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./calculator.scss"
 
 export default function Calculator() {
+	const [currentExpression, setCurrentExpression] = useState([])
+	const [currentTotal, setCurrentTotal] = useState(0)
+
+	// On number or decimal input, add to currentInput array
+	// On operator input, add currentInput to currentExpression array and clear input
+	const mathLookup = {
+		zero: 0,
+		one: 1,
+		two: 2,
+		three: 3,
+		four: 4,
+		five: 5,
+		six: 6,
+		seven: 7,
+		eight: 8,
+		nine: 9,
+		decimal: ".",
+		add: "+",
+		subtract: "-",
+		multiply: "*",
+		divide: "/",
+		equals: "=",
+		factorial: "!",
+	}
+
+	function backspaceInput() {
+		setCurrentInput(currentExpression.slice(0, -1))
+	}
+
+	function resetCalculator() {
+		setCurrentExpression([])
+		setCurrentTotal(0)
+	}
+
+	function solveExpression() {
+		console.log(currentExpression)
+	}
+
+	function handleClickInput(event) {
+		// Set input to alphabetic value
+		const input = event.target.id
+
+		if (mathLookup[input]) {
+			setCurrentExpression([...currentExpression, mathLookup[input]])
+		} else {
+			console.log("Not a valid input.")
+		}
+		console.log(currentInput.join(","))
+	}
+
+	// On equals input, parse array according to order of operations
 	return (
 		<div className="calculator" id="calculator">
-			<div className="display"></div>
+			<div className="display" id="display">
+				{currentExpression.join("")}
+			</div>
 			<div className="button-container">
 				<div className="top-buttons">
 					<div className="calc-label">CALC</div>
-					<button className="top-button" id="clear">
+					<button className="top-button" id="clear" onClick={resetCalculator}>
 						C
 					</button>
-					<button className="top-button" id="backspace">
+					<button
+						className="top-button"
+						id="backspace"
+						onClick={backspaceInput}
+					>
 						B
 					</button>
 				</div>
 				<div className="numpad">
-					<button className="num-button" id="nine">
+					<button className="num-button" id="nine" onClick={handleClickInput}>
 						9
 					</button>
-					<button className="num-button" id="eight">
+					<button className="num-button" id="eight" onClick={handleClickInput}>
 						8
 					</button>
-					<button className="num-button" id="seven">
+					<button className="num-button" id="seven" onClick={handleClickInput}>
 						7
 					</button>
-					<button className="num-button" id="six">
+					<button className="num-button" id="six" onClick={handleClickInput}>
 						6
 					</button>
-					<button className="num-button" id="five">
+					<button className="num-button" id="five" onClick={handleClickInput}>
 						5
 					</button>
-					<button className="num-button" id="four">
+					<button className="num-button" id="four" onClick={handleClickInput}>
 						4
 					</button>
-					<button className="num-button" id="three">
+					<button className="num-button" id="three" onClick={handleClickInput}>
 						3
 					</button>
-					<button className="num-button" id="two">
+					<button className="num-button" id="two" onClick={handleClickInput}>
 						2
 					</button>
-					<button className="num-button" id="one">
+					<button className="num-button" id="one" onClick={handleClickInput}>
 						1
 					</button>
-					<button className="num-button" id="factorial">
+					<button
+						className="num-button"
+						id="factorial"
+						onClick={handleClickInput}
+					>
 						!
 					</button>
-					<button className="num-button" id="zero">
+					<button className="num-button" id="zero" onClick={handleClickInput}>
 						0
 					</button>
-					<button className="num-button" id="decimal">
+					<button
+						className="num-button"
+						id="decimal"
+						onClick={handleClickInput}
+					>
 						.
 					</button>
 				</div>
 				<div className="sympad">
-					<button className="sym-button" id="exp">
+					<button className="sym-button" id="exp" onClick={handleClickInput}>
 						x<sup>2</sup>
 					</button>
-					<button className="sym-button" id="sqrt">
+					<button className="sym-button" id="sqrt" onClick={handleClickInput}>
 						&#8730;
 					</button>
-					<button className="sym-button" id="multiply">
+					<button
+						className="sym-button"
+						id="multiply"
+						onClick={handleClickInput}
+					>
 						&#215;
 					</button>
-					<button className="sym-button" id="divide">
+					<button className="sym-button" id="divide" onClick={handleClickInput}>
 						&#247;
 					</button>
-					<button className="sym-button" id="add">
+					<button className="sym-button" id="add" onClick={handleClickInput}>
 						&#43;
 					</button>
-					<button className="sym-button" id="subtract">
+					<button
+						className="sym-button"
+						id="subtract"
+						onClick={handleClickInput}
+					>
 						&#8722;
 					</button>
-					<button className="sym-button" id="equals">
+					<button className="sym-button" id="equals" onClick={solveExpression}>
 						&#61;
 					</button>
 				</div>
