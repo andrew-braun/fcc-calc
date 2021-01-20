1ddfadd734238d1527c4aea721d7688eafcd276a
Andrew Braun <andrew@andrewbraun.dev>
2021-01-20 19:47:18 +0400

    style: Add math symbols, layout fixes

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -8,7 +8,7 @@ export default function Calculator() {
 			<div className="button-container">
 				<div className="top-buttons">
 					<div className="calc-label">CALC</div>
-					<button className="top-button" id="C">
+					<button className="top-button" id="clear">
 						C
 					</button>
 					<button className="top-button" id="backspace">
@@ -16,64 +16,64 @@ export default function Calculator() {
 					</button>
 				</div>
 				<div className="numpad">
-					<button className="num-button" id="zero">
-						0
-					</button>
-					<button className="num-button" id="one">
-						1
-					</button>
-					<button className="num-button" id="two">
-						2
-					</button>
-					<button className="num-button" id="three">
-						3
-					</button>
-					<button className="num-button" id="four">
-						4
-					</button>
-					<button className="num-button" id="five">
-						5
-					</button>
-					<button className="num-button" id="six">
-						6
-					</button>
-					<button className="num-button" id="seven">
-						7
+					<button className="num-button" id="nine">
+						9
 					</button>
 					<button className="num-button" id="eight">
 						8
 					</button>
+					<button className="num-button" id="seven">
+						7
+					</button>
+					<button className="num-button" id="six">
+						6
+					</button>
+					<button className="num-button" id="five">
+						5
+					</button>
+					<button className="num-button" id="four">
+						4
+					</button>
+					<button className="num-button" id="three">
+						3
+					</button>
+					<button className="num-button" id="two">
+						2
+					</button>
+					<button className="num-button" id="one">
+						1
+					</button>
 					<button className="num-button" id="factorial">
 						!
 					</button>
-					<button className="num-button" id="0">
+					<button className="num-button" id="zero">
 						0
 					</button>
-					<button className="num-button" id=".">
+					<button className="num-button" id="decimal">
 						.
 					</button>
 				</div>
 				<div className="sympad">
 					<button className="sym-button" id="exp">
-						x2
+						x<sup>2</sup>
 					</button>
 					<button className="sym-button" id="sqrt">
-						sq
+						&#8730;
 					</button>
 					<button className="sym-button" id="multiply">
-						x
+						&#215;
 					</button>
 					<button className="sym-button" id="divide">
-						/
+						&#247;
 					</button>
 					<button className="sym-button" id="add">
-						+
+						&#43;
 					</button>
 					<button className="sym-button" id="subtract">
-						-
+						&#8722;
 					</button>
 					<button className="sym-button" id="equals">
-						=
+						&#61;
 					</button>
 				</div>
 			</div>
