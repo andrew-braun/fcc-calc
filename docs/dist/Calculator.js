9903f7df6dc7b6da984926279277b52f3a21c284
Andrew Braun <andrew@andrewbraun.dev>
2021-01-20 19:47:57 +0400

    build: Add updated Snowpack build

--- docs/dist/Calculator.js
+++ docs/dist/Calculator.js
@@ -14,7 +14,7 @@ export default function Calculator() {
     className: "calc-label"
   }, "CALC"), /* @__PURE__ */ React.createElement("button", {
     className: "top-button",
-    id: "C"
+    id: "clear"
   }, "C"), /* @__PURE__ */ React.createElement("button", {
     className: "top-button",
     id: "backspace"
@@ -22,61 +22,61 @@ export default function Calculator() {
     className: "numpad"
   }, /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "zero"
-  }, "0"), /* @__PURE__ */ React.createElement("button", {
-    className: "num-button",
-    id: "one"
-  }, "1"), /* @__PURE__ */ React.createElement("button", {
-    className: "num-button",
-    id: "two"
-  }, "2"), /* @__PURE__ */ React.createElement("button", {
-    className: "num-button",
-    id: "three"
-  }, "3"), /* @__PURE__ */ React.createElement("button", {
-    className: "num-button",
-    id: "four"
-  }, "4"), /* @__PURE__ */ React.createElement("button", {
-    className: "num-button",
-    id: "five"
-  }, "5"), /* @__PURE__ */ React.createElement("button", {
-    className: "num-button",
-    id: "six"
-  }, "6"), /* @__PURE__ */ React.createElement("button", {
-    className: "num-button",
-    id: "seven"
-  }, "7"), /* @__PURE__ */ React.createElement("button", {
+    id: "nine"
+  }, "9"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
     id: "eight"
   }, "8"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
+    id: "seven"
+  }, "7"), /* @__PURE__ */ React.createElement("button", {
+    className: "num-button",
+    id: "six"
+  }, "6"), /* @__PURE__ */ React.createElement("button", {
+    className: "num-button",
+    id: "five"
+  }, "5"), /* @__PURE__ */ React.createElement("button", {
+    className: "num-button",
+    id: "four"
+  }, "4"), /* @__PURE__ */ React.createElement("button", {
+    className: "num-button",
+    id: "three"
+  }, "3"), /* @__PURE__ */ React.createElement("button", {
+    className: "num-button",
+    id: "two"
+  }, "2"), /* @__PURE__ */ React.createElement("button", {
+    className: "num-button",
+    id: "one"
+  }, "1"), /* @__PURE__ */ React.createElement("button", {
+    className: "num-button",
     id: "factorial"
   }, "!"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "0"
+    id: "zero"
   }, "0"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "."
+    id: "decimal"
   }, ".")), /* @__PURE__ */ React.createElement("div", {
     className: "sympad"
   }, /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
     id: "exp"
-  }, "x2"), /* @__PURE__ */ React.createElement("button", {
+  }, "x", /* @__PURE__ */ React.createElement("sup", null, "2")), /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
     id: "sqrt"
-  }, "sq"), /* @__PURE__ */ React.createElement("button", {
+  }, "\u221A"), /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
     id: "multiply"
-  }, "x"), /* @__PURE__ */ React.createElement("button", {
+  }, "\xD7"), /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
     id: "divide"
-  }, "/"), /* @__PURE__ */ React.createElement("button", {
+  }, "\xF7"), /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
     id: "add"
   }, "+"), /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
     id: "subtract"
-  }, "-"), /* @__PURE__ */ React.createElement("button", {
+  }, "\u2212"), /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
     id: "equals"
   }, "="))));
