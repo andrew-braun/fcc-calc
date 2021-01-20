4075a8372c80ec833df4c18348defaf0c12b813d
Andrew Braun <andrew@andrewbraun.dev>
2021-01-20 13:51:32 +0400

    build: Add global css

--- docs/dist/App.js
+++ docs/dist/App.js
@@ -1,6 +1,7 @@
 import React from "../_snowpack/pkg/react.js";
 import ReactDOM from "../_snowpack/pkg/react-dom.js";
 import Calculator from "./Calculator.js";
+import "./styles/global.css.proxy.js";
 import "./app.css.proxy.js";
 export default function App() {
   return /* @__PURE__ */ React.createElement("div", {
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import Calculator from "./Calculator"
+import "./styles/global.css"
 import "./app.scss"
 
 export default function App() {
