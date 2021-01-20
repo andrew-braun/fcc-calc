0bb039de653544d50f88034aae54d84dd00a2cff
Andrew Braun <andrew@andrewbraun.dev>
2021-01-20 22:40:59 +0400

    build: Rebuild snowpack dist

--- docs/dist/Calculator.js
+++ docs/dist/Calculator.js
@@ -1,12 +1,53 @@
-import React from "../_snowpack/pkg/react.js";
+import React, {useState, useEffect} from "../_snowpack/pkg/react.js";
 import "./calculator.css.proxy.js";
 export default function Calculator() {
+  const [currentExpression, setCurrentExpression] = useState([]);
+  const [currentTotal, setCurrentTotal] = useState(0);
+  const mathLookup = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    decimal: ".",
+    add: "+",
+    subtract: "-",
+    multiply: "*",
+    divide: "/",
+    equals: "=",
+    factorial: "!"
+  };
+  function backspaceInput() {
+    setCurrentInput(currentExpression.slice(0, -1));
+  }
+  function resetCalculator() {
+    setCurrentExpression([]);
+    setCurrentTotal(0);
+  }
+  function solveExpression() {
+    console.log(currentExpression);
+  }
+  function handleClickInput(event) {
+    const input = event.target.id;
+    if (mathLookup[input]) {
+      setCurrentExpression([...currentExpression, mathLookup[input]]);
+    } else {
+      console.log("Not a valid input.");
+    }
+    console.log(currentInput.join(","));
+  }
   return /* @__PURE__ */ React.createElement("div", {
     className: "calculator",
     id: "calculator"
   }, /* @__PURE__ */ React.createElement("div", {
-    className: "display"
-  }), /* @__PURE__ */ React.createElement("div", {
+    className: "display",
+    id: "display"
+  }, currentExpression.join("")), /* @__PURE__ */ React.createElement("div", {
     className: "button-container"
   }, /* @__PURE__ */ React.createElement("div", {
     className: "top-buttons"
@@ -14,70 +55,91 @@ export default function Calculator() {
     className: "calc-label"
   }, "CALC"), /* @__PURE__ */ React.createElement("button", {
     className: "top-button",
-    id: "clear"
+    id: "clear",
+    onClick: resetCalculator
   }, "C"), /* @__PURE__ */ React.createElement("button", {
     className: "top-button",
-    id: "backspace"
+    id: "backspace",
+    onClick: backspaceInput
   }, "B")), /* @__PURE__ */ React.createElement("div", {
     className: "numpad"
   }, /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "nine"
+    id: "nine",
+    onClick: handleClickInput
   }, "9"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "eight"
+    id: "eight",
+    onClick: handleClickInput
   }, "8"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "seven"
+    id: "seven",
+    onClick: handleClickInput
   }, "7"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "six"
+    id: "six",
+    onClick: handleClickInput
   }, "6"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "five"
+    id: "five",
+    onClick: handleClickInput
   }, "5"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "four"
+    id: "four",
+    onClick: handleClickInput
   }, "4"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "three"
+    id: "three",
+    onClick: handleClickInput
   }, "3"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "two"
+    id: "two",
+    onClick: handleClickInput
   }, "2"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "one"
+    id: "one",
+    onClick: handleClickInput
   }, "1"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "factorial"
+    id: "factorial",
+    onClick: handleClickInput
   }, "!"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "zero"
+    id: "zero",
+    onClick: handleClickInput
   }, "0"), /* @__PURE__ */ React.createElement("button", {
     className: "num-button",
-    id: "decimal"
+    id: "decimal",
+    onClick: handleClickInput
   }, ".")), /* @__PURE__ */ React.createElement("div", {
     className: "sympad"
   }, /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
-    id: "exp"
+    id: "exp",
+    onClick: handleClickInput
   }, "x", /* @__PURE__ */ React.createElement("sup", null, "2")), /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
-    id: "sqrt"
+    id: "sqrt",
+    onClick: handleClickInput
   }, "\u221A"), /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
-    id: "multiply"
+    id: "multiply",
+    onClick: handleClickInput
   }, "\xD7"), /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
-    id: "divide"
+    id: "divide",
+    onClick: handleClickInput
   }, "\xF7"), /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
-    id: "add"
+    id: "add",
+    onClick: handleClickInput
   }, "+"), /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
-    id: "subtract"
+    id: "subtract",
+    onClick: handleClickInput
   }, "\u2212"), /* @__PURE__ */ React.createElement("button", {
     className: "sym-button",
-    id: "equals"
+    id: "equals",
+    onClick: solveExpression
   }, "="))));
 }
